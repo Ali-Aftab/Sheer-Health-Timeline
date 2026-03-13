a9b4908038df981a39a1c9e71476278120b98cd2
Fig Yogurt <itisaftab@gmail.com>
2026-03-13 00:44:32 -0400

    feat(eventinfo): :sparkles: added more details to eventinfo

--- src/components/EventInfo.jsx
+++ src/components/EventInfo.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 const EventInfo = ({ curEvent }) => {
+  const [curTab, setCurTab] = useState("");
+
   if (Object.keys(curEvent).length)
     return (
       <div className="event-info">
@@ -8,10 +10,21 @@ const EventInfo = ({ curEvent }) => {
       </div>
     );
 
-  const [curTab, setCurTab] = useState("");
+  const {
+    event_type,
+    details,
+    create_time,
+    messages,
+    supportNotes,
+    attachments,
+    bills,
+  } = curEvent;
 
   return (
     <div className="event-info">
+      <h1>{event_type}</h1>
+      <h2>{details}</h2>
+      <h3>{new Date(create_time).toLocaleString()}</h3>
       <button onClick={() => setCurTab("messages")}>Messages</button>
       <button onClick={() => setCurTab("supportNotes")}>Support Notes</button>
       <button onClick={() => setCurTab("attachments")}>Attachments</button>
