9e0c3cbaf876e1bec902f7f687086d01d4852a24
Fig Yogurt <itisaftab@gmail.com>
2026-03-15 03:16:12 -0400

    ui(attachments): :lipstick: created ui for attachments

--- src/components/Attachments.jsx
+++ src/components/Attachments.jsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 const Attachments = ({ list, handleList }) => {
   const [fileName, setFileName] = useState("");
   const [inputKey, setInputKey] = useState(0);
-  console.log(list);
 
   const handleFileSave = (e) => {
     setFileName(e.target.files[0].name);
   };
 
-  const handleFileUpload = (e) => {
+  const handleFileUpload = () => {
     if (!fileName) return alert("Upload a file!");
     handleList("attachment", { file_name: fileName });
     setFileName("");
@@ -17,21 +16,26 @@ const Attachments = ({ list, handleList }) => {
   };
 
   return (
-    <div>
-      <h4>Attachments</h4>
-      {list.length > 0
-        ? list.map((attachment) => (
-            <div key={attachment.attachment_id}>
-              <h6>File Name: {attachment.file_name}</h6>
-            </div>
-          ))
-        : ""}
-
-      <br />
-      <h4>Upload a file</h4>
-      <input type="file" onChange={handleFileSave} key={inputKey} />
-      <br />
-      <button onClick={handleFileUpload}>Add File</button>
+    <div className="tab-content">
+      <p className="tab-title">Attachments</p>
+      {list.length > 0 ? (
+        list.map((attachment) => (
+          <div key={attachment.attachment_id} className="tab-item">
+            <p className="tab-item-text">{attachment.file_name}</p>
+          </div>
+        ))
+      ) : (
+        <p className="tab-item-label">No attachments</p>
+      )}
+      <input
+        className="file-input"
+        type="file"
+        onChange={handleFileSave}
+        key={inputKey}
+      />
+      <button className="tab-btn-submit" onClick={handleFileUpload}>
+        Add File
+      </button>
     </div>
   );
 };
