ff94e4e1d33ff0241a0a317e87fcf4df1f347d0e
Fig Yogurt <itisaftab@gmail.com>
2026-03-15 02:31:36 -0400

    ui(eventinfo): :lipstick: made better styling choices on event info and add ui for the tab buttons

--- src/components/EventInfo.jsx
+++ src/components/EventInfo.jsx
@@ -10,7 +10,7 @@ const EventInfo = ({ curEvent, handleList }) => {
   if (!Object.keys(curEvent).length)
     return (
       <div className="event-info">
-        <h1>Select an event!</h1>
+        <p className="event-type">Select an event!</p>
       </div>
     );
 
@@ -26,14 +26,35 @@ const EventInfo = ({ curEvent, handleList }) => {
 
   return (
     <div className="event-info">
-      <h1>{event_type}</h1>
-      <h2>{details}</h2>
-      <h3>{new Date(create_time).toLocaleString()}</h3>
-      <button onClick={() => setCurTab("messages")}>Messages</button>
-      <button onClick={() => setCurTab("supportNotes")}>Support Notes</button>
-      <button onClick={() => setCurTab("attachments")}>Attachments</button>
-      <button onClick={() => setCurTab("bills")}>Bills</button>
-
+      <p className="event-type">{event_type}</p>
+      <p className="event-details">{details}</p>
+      <p className="event-date">{new Date(create_time).toLocaleString()}</p>
+      <div className="tab-bar">
+        <button
+          className={`tab-btn ${curTab === "messages" ? "tab-active" : ""}`}
+          onClick={() => setCurTab("messages")}
+        >
+          Messages
+        </button>
+        <button
+          className={`tab-btn ${curTab === "supportNotes" ? "tab-active" : ""}`}
+          onClick={() => setCurTab("supportNotes")}
+        >
+          Support Notes
+        </button>
+        <button
+          className={`tab-btn ${curTab === "attachments" ? "tab-active" : ""}`}
+          onClick={() => setCurTab("attachments")}
+        >
+          Attachments
+        </button>
+        <button
+          className={`tab-btn ${curTab === "bills" ? "tab-active" : ""}`}
+          onClick={() => setCurTab("bills")}
+        >
+          Bills
+        </button>
+      </div>
       {curTab === "attachments" ? (
         <Attachments list={attachments} handleList={handleList} />
       ) : (
