093563ea9a7cc2849d9ab3ad721fc0792d497c21
Fig Yogurt <itisaftab@gmail.com>
2026-03-15 02:55:42 -0400

    ui(app): :lipstick: created ui for create event form

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -21,20 +21,37 @@ const CreateEvent = ({ createOneEvent, setCreatingEvent }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <select value={eventType} onChange={(e) => setEventType(e.target.value)}>
-        {choices.map((choice, ind) => (
-          <option key={`choice${ind}`}>{choice}</option>
-        ))}
-      </select>
-      <br />
-      <textarea onChange={(e) => setEventDetail(e.target.value)} />
-      <br />
-      <button type="submit">Send</button>
-      <button type="button" onClick={() => setCreatingEvent(false)}>
-        Cancel
-      </button>
-    </form>
+    <div className="event-info">
+      <p className="event-type">Create a New Event!</p>
+      <form className="create-event-form" onSubmit={handleSubmit}>
+        <select
+          className="tab-input"
+          value={eventType}
+          onChange={(e) => setEventType(e.target.value)}
+        >
+          {choices.map((choice, ind) => (
+            <option key={`choice${ind}`}>{choice}</option>
+          ))}
+        </select>
+        <textarea
+          className="tab-input create-event-textarea"
+          onChange={(e) => setEventDetail(e.target.value)}
+          placeholder="Event details..."
+        />
+        <div className="create-event-actions">
+          <button className="tab-btn-submit" type="submit">
+            Create
+          </button>
+          <button
+            className="tab-btn-cancel"
+            type="button"
+            onClick={() => setCreatingEvent(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
